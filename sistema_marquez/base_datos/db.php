d00e3a2201548095b42e10b3b0b23cafa069665f
GimenezAlfredo <alfredogimenez431@gmail.com>
2024-10-07 21:11:20 -0300

    cambios_rama

--- sistema_marquez/base_datos/db.php
+++ sistema_marquez/base_datos/db.php
@@ -2,7 +2,7 @@
 // Configuración de la base de datos: se establecen los parámetros para la conexión a la base de datos MySQL.
 $servername = "localhost:3308"; // Nombre del servidor donde se aloja la base de datos.
 $username = "root";        // Nombre de usuario para acceder a la base de datos.
-$password = "543210";      // Contraseña del usuario para acceder a la base de datos.
+$password = "";      // Contraseña del usuario para acceder a la base de datos.
 $dbname = "pruebas_marquez2"; // Nombre de la base de datos a la cual se quiere conectar.
 
 // Crear conexión: se crea un nuevo objeto mysqli para conectarse a la base de datos utilizando los parámetros definidos.
